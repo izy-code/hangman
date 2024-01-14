b2505cbc654d708c6891d15bd224dffcace59bf7
Izy <126877709+izy-code@users.noreply.github.com>
2024-01-14 14:49:46 +0300

    fix: replace alert message with console.log

--- hangman/js/main.js
+++ hangman/js/main.js
@@ -4,6 +4,8 @@ import { quizNode } from './quiz.js';
 import { modalNode } from './modal.js';
 import { startGame } from './game.js';
 
+console.log('Ensure your keyboard is set to the English layout.');
+
 const mainNode = createNode('main', 'main-content');
 const headingNode = createNode('h1', 'main-content__title');
 
@@ -13,6 +15,3 @@ mainNode.append(headingNode, gallowsNode, quizNode, modalNode);
 
 document.body.append(mainNode);
 
-setTimeout(() => {
-  alert('Ensure your keyboard is set to the English layout.');
-}, 250);
