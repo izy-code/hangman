562fccc38aa872cf4f8d4b4fb189423654a31d2e
Izy <126877709+izy-code@users.noreply.github.com>
2024-01-14 12:51:34 +0300

    feat: add game questions and answers

---
+++ hangman/js/data.js
@@ -0,0 +1,107 @@
+const data = [
+  {
+    answer: 'JavaScript',
+    question:
+      'What is the most widely used programming language for web development?',
+  },
+  {
+    answer: 'HTML',
+    question: 'Which markup language is used to structure content on the web?',
+  },
+  {
+    answer: 'CSS',
+    question: 'What is the styling language used for designing web pages?',
+  },
+  {
+    answer: 'Browser',
+    question:
+      'What software application is commonly used to access and navigate the internet?',
+  },
+  {
+    answer: 'SEO',
+    question:
+      'What acronym refers to the practice of optimizing websites to rank higher in search engine results?',
+  },
+  {
+    answer: 'GitHub',
+    question:
+      'Which platform is widely used for version control and collaborative coding projects?',
+  },
+  {
+    answer: 'AJAX',
+    question:
+      'What technology allows asynchronous communication between a web browser and a server?',
+  },
+  {
+    answer: 'JSON',
+    question:
+      'What format is commonly used for data interchange between a server and a web application?',
+  },
+  {
+    answer: 'DNS',
+    question:
+      'What system translates domain names to IP addresses on the internet?',
+  },
+  {
+    answer: 'Hosting',
+    question:
+      'What term refers to the service that allows individuals and organizations to make their websites accessible via the World Wide Web?',
+  },
+  {
+    answer: 'Security',
+    question:
+      'What aspect of web development focuses on protecting websites and web applications from cyber threats?',
+  },
+  {
+    answer: 'Grid',
+    question:
+      'What CSS layout system allows the design of complex web layouts with rows and columns?',
+  },
+  {
+    answer: 'Sass',
+    question:
+      'Which CSS preprocessor extends the functionality of CSS by providing features like variables and nested rules?',
+  },
+  {
+    answer: 'HTTPS',
+    question:
+      'What protocol is used for secure data communication over a computer network, commonly used in web browsing?',
+  },
+  {
+    answer: 'Closure',
+    question:
+      'What term in JavaScript refers to the ability of a function to access variables from its outer scope even after the function has finished execution?',
+  },
+  {
+    answer: 'Callback',
+    question:
+      'What is a function passed as an argument to another function, which will be invoked later in the program execution process?',
+  },
+  {
+    answer: 'Promise',
+    question:
+      'What is an object in JavaScript representing the eventual completion or failure of an asynchronous operation?',
+  },
+  {
+    answer: 'Hoisting',
+    question:
+      'What is a behavior in JavaScript where variable and function declarations are moved to the top of their containing scope during the compilation phase?',
+  },
+  {
+    answer: 'Event',
+    question:
+      'What term in JavaScript refers to an action or occurrence detected by the program that can be responded to, such as a user clicking a button?',
+  },
+  {
+    answer: 'Arrow',
+    question:
+      'What concise syntax introduced in ECMAScript 6 allows the creation of anonymous functions more easily, often used for short, one-line functions?',
+  },
+  {
+    answer: 'Truthy',
+    question:
+      'What term in JavaScript describes a value that is considered true when evaluated in a Boolean context, even if it is not a strict Boolean true value?',
+  },
+];
+
+export { data };
